737035eb4d15cc4e01f153f3c1f005301b338a05
Hieu Ngo (Henry) <hngo1@umassd.edu>
2026-01-29 19:15:33 -0500

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,3 @@
+document.getElementById('year').textContent = new Date().getFullYear();
+// If you host your CV at /cv/Henry_Ngo_CV.pdf, update the link below:
+document.getElementById('cv-link').setAttribute('href','cv/Henry_Ngo_CV.pdf');
